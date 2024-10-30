1c74b3710507350de779738324b67dd9219ccb4a
klnstprx <ignacy.borzestowski@gmail.com>
2024-10-30 17:51:40 +0100

    added readme, removed .env (secrets cant be shared on repo)

--- src/backend/wiki/database/wiki_database.go
+++ src/backend/wiki/database/wiki_database.go
@@ -19,6 +19,7 @@ func Connect() {
 	mongoURI := os.Getenv("MONGODB_URI")
 	if mongoURI == "" {
 		mongoURI = "mongodb://localhost:27017"
+		config.App.Logger.Warn().Str("mongoURI", mongoURI).Msg("MongoURI not set in .env, defaulting to locally hosted DB.")
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -39,5 +40,5 @@ func Connect() {
 	Client = client
 	WikiCollection = client.Database("lawikidb").Collection("wikis")
 
-	config.App.Logger.Info().Msg("Connected to mongoDB")
+	config.App.Logger.Info().Str("mongoURI", mongoURI).Msg("Connected to mongoDB")
 }
