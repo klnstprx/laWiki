be0b5ac8bb2c3e67c05f8e8166d292db6a6d55a4
mikzb <mikolajzabski@uma.es>
2024-11-06 13:43:13 +0100

    fixed typos

--- src/backend/entry/main.go
+++ src/backend/entry/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"crypto/internal/boring/sig"
+	"context"
+	"errors"
 	"net/http"
+	"os"
 	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/laWiki/entry/config"
+	"github.com/laWiki/entry/database"
 	"github.com/rs/zerolog/log"
 )
 
@@ -21,17 +26,17 @@ func main() {
 	xlog.Info().Msg("Connecting to the database...")
 	database.Connect()
 
+	//router setup, no need to mount cause only 1 router
 	r := chi.NewRouter()
-	r.Mount(/entries, router.NewRouter())
 
-	ctx,cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// graceful shutdown logic
-	signalCaught := false;
-	signalChannel := make(chan os.signal,1)
-	signal.Notify(signalChannel,os.Interrupt,syscall.SIGTERM)
-	go func(){
+	signalCaught := false
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+	go func() {
 		<-signalChannel
 		if signalCaught {
 			xlog.Warn().Msg("Caught second signal, terminating immediately")
@@ -42,28 +47,27 @@ func main() {
 		cancel()
 	}()
 
-
 	//server starup
 	httpServer := http.Server{
-		Addr: config.App.Port
+		Addr:    config.App.Port,
 		Handler: r,
 	}
 
 	go func() {
-		err:=httpServer.ListenAndServe()
-		if err !=nil&&errores.Is(err,http.ErrServerClosed){
+		err := httpServer.ListenAndServe()
+		if err != nil && errors.Is(err, http.ErrServerClosed) {
 			xlog.Fatal().Err(err).Msg("Failed to start HTTP server")
 		}
-	}
-	xlog.Info().Str("port",config.App.Port).Msg("HTTP Server started")
+	}()
+	xlog.Info().Str("port", config.App.Port).Msg("HTTP Server started")
 
 	//wait for shutdown signal
 	<-ctx.Done()
 
 	//shutdown logic
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer shutdownCancel()
-	if err:=httpServer.Shutdown(shutdownCtx);err!=nil{
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
 		xlog.Fatal().Err(err).Msg("Failed to gracefully shutdown HTTP server")
 	}
 	xlog.Info().Msg("HTTP server shut down successfully")
