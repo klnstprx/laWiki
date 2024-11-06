b7b3c782da1e96cbf81cd94da6cad4dfed806ac2
Olivier <oliviergg@uma.es>
2024-11-06 18:10:35 +0100

    Funciona todo

--- src/backend/version/router/version_router.go
+++ src/backend/version/router/version_router.go
@@ -14,13 +14,12 @@ func NewRouter() http.Handler {
 		r.Get("/", handler.GetVersions)
 		r.Post("/", handler.PostVersion)
 
-		/*
-			r.Route("/{id}", func(r chi.Router) {
-				r.Get("/", handler.GetWikiByID)
-				r.Put("/", handler.PutWiki)
-				r.Delete("/", handler.DeleteWiki)
-			})
-		*/
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", handler.GetVersionByID)
+			r.Put("/", handler.PutVersion)
+			r.Delete("/", handler.DeleteVersion)
+		})
+
 	})
 
 	return r
