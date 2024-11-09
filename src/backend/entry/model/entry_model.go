8246cd95d023d1b9441fbe4564e9d8a64d0f1637
Olivier <oliviergg@uma.es>
2024-11-09 14:46:18 +0100

    autores cambiado a autor en entrada

--- src/backend/entry/model/entry_model.go
+++ src/backend/entry/model/entry_model.go
@@ -7,6 +7,6 @@ import (
 type Entry struct {
 	ID        string    `json:"id" bson:"_id,omitempty"`
 	Title     string    `json:"title" bson:"title"`
-	Authors   []string  `json:"authors" bson:"authors"`
+	Authors   string    `json:"authors" bson:"authors"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 }
