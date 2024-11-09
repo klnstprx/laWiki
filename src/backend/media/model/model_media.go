4c98faaecdf3f572365ac85cdf2b3af55a5d4ad7
Croissanton <croiss@CRISTIANPC.>
2024-11-09 14:10:15 +0100

    putmedia works now

--- src/backend/media/model/model_media.go
+++ src/backend/media/model/model_media.go
@@ -3,5 +3,4 @@ package model
 type Media struct {
 	ID        string `json:"id" bson:"_id,omitempty"`
 	UploadUrl string `json:"uploadUrl" bson:"uploadUrl"`
-	AssetUrl  string `json:"assetUrl" bson:"assetUrl"`
 }
