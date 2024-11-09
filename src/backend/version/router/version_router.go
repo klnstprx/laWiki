12455b8ba67f30b9baf670d0e54d7489cfda37df
Olivier <oliviergg@uma.es>
2024-11-09 14:19:53 +0100

    / eliminada del router

--- src/backend/version/router/version_router.go
+++ src/backend/version/router/version_router.go
@@ -14,10 +14,10 @@ func NewRouter() http.Handler {
 		r.Get("/health", handler.HealthCheck)
 		r.Get("/", handler.GetVersions)
 		r.Post("/", handler.PostVersion)
-		r.Get("/entry/", handler.GetVersionsByEntryID)
-		r.Get("/content/", handler.GetVersionsByContent)
-		r.Get("/editor/", handler.GetVersionsByEditor)
-		r.Get("/date/", handler.GetVersionsByDate)
+		r.Get("/entry", handler.GetVersionsByEntryID)
+		r.Get("/content", handler.GetVersionsByContent)
+		r.Get("/editor", handler.GetVersionsByEditor)
+		r.Get("/date", handler.GetVersionsByDate)
 
 		r.Route("/id", func(r chi.Router) {
 			r.Get("/", handler.GetVersionByID)
