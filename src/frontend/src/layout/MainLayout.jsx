c3cd7018c07764d1b509cb01b89ac9af5258544c
Pablo <pabloruar03@uma.es>
2024-11-22 19:13:10 +0100

    MainLayout, Tostadas y carpetas

---
+++ src/frontend/src/layout/MainLayout.jsx
@@ -0,0 +1,57 @@
+import { useState, useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import ToastMessage from "../Components/ToastMessage";
+import { useToast } from "../Context/ToastContext";
+import { Outlet } from "react-router-dom";
+
+function MainLayout({ children }) {
+  const controlRef = useRef(null);
+  const [topOffset, setTopOffset] = useState(0);
+
+  useEffect(() => {
+    const updatePadding = () => {
+      if (controlRef.current) {
+        setTopOffset(controlRef.current.offsetHeight);
+      }
+    };
+
+    updatePadding();
+    window.addEventListener("resize", updatePadding);
+    return () => window.removeEventListener("resize", updatePadding);
+  }, []);
+
+  return (
+    <div className="d-flex flex-column vh-100">
+      <Header ref={controlRef} />
+      <div
+        className="flex-grow-1 pb-5"
+        style={{ paddingTop: `${topOffset}px` }}
+      >
+        {children}
+        <Outlet />
+        <ToastMessagesLayout />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+const ToastMessagesLayout = () => {
+  const { toast, hideToast } = useToast();
+
+  return (
+    <ToastMessage
+      show={toast.show}
+      onClose={hideToast}
+      message={toast.message}
+      color={toast.color}
+    />
+  );
+};
+MainLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default MainLayout;
