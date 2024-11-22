069aa1907c466896caff3c5b429bf37fe8803647
Pablo <pabloruar03@uma.es>
2024-11-22 19:13:10 +0100

    MainLayout, Tostadas y carpetas

---
+++ src/frontend/src/components/ToastMessage.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { Toast } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+function ToastMessage({
+  show,
+  message,
+  onClose,
+  delay = 3000,
+  color = "bg-info",
+}) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, delay);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [show, onClose, delay]);
+
+  return (
+    <Toast
+      show={show}
+      onClose={onClose}
+      delay={delay}
+      autohide
+      className={`${color}`}
+      style={{
+        position: "fixed",
+        top: "20px",
+        right: "20px",
+        zIndex: 9999,
+      }}
+    >
+      <Toast.Header>
+        <strong className="m-auto">Notificacion</strong>
+      </Toast.Header>
+      <Toast.Body style={{ color: "black" }}>{message}</Toast.Body></Toast>
+    );
+}
+
+ToastMessage.propTypes = {
+  show: PropTypes.bool.isRequired,
+  message: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+  delay: PropTypes.number,
+  color: PropTypes.string,
+};
+
+export default ToastMessage;
---
+++ src/frontend/src/context/ToastContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+import PropTypes from 'prop-types';
+
+export const ToastContext = createContext();
+
+export const ToastProvider = ({ children }) => {
+  const [toast, setToast] = useState({
+    show: false,
+    message: "",
+    color: "bg-info",
+  });
+
+  const showToast = (message, color = "bg-info") => {
+    setToast({ show: true, message, color });
+  };
+
+  const hideToast = () => {
+    setToast({ ...toast, show: false });
+  };
+
+  return (
+    <ToastContext.Provider value={{ toast, showToast, hideToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+};
+
+ToastProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
