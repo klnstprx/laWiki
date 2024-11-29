f303055b6b71cf7d3fddfe4161f3ad21db8cf4f2
mikzb <mikolajzabski@uma.es>
2024-11-29 18:45:29 +0100

    new header and footer

--- src/frontend/src/pages/HomePage.jsx
+++ src/frontend/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import ListItem from "@mui/material/ListItem";
 import Alert from "@mui/material/Alert";
 import Typography from "@mui/material/Typography";
 import { getAllWikis } from "../api.js";
+import MainLayout from "../layout/MainLayout.jsx";
+
 
 function HomePage() {
   const [wikis, setWikis] = useState([]);
@@ -16,28 +18,30 @@ function HomePage() {
   }, []);
 
   return (
-    <div>
-      <Typography variant="h1" gutterBottom>
-        Wikis
-      </Typography>
-      {error && <Alert severity="error">{error}</Alert>}
-      {wikis.length > 0
-        ? (
-          <List>
-            {wikis.map((wiki) => (
-              <ListItem key={wiki.id}>
-                <Typography variant="h2" gutterBottom>
-                  {wiki.title}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                  {wiki.description}
-                </Typography>
-              </ListItem>
-            ))}
-          </List>
-        )
-        : <Alert>No wikis found.</Alert>}
-    </div>
+    <MainLayout>
+      <div>
+        <Typography variant="h1" gutterBottom>
+          Wikis
+        </Typography>
+        {error && <Alert severity="error">{error}</Alert>}
+        {wikis.length > 0
+          ? (
+            <List>
+              {wikis.map((wiki) => (
+                <ListItem key={wiki.id}>
+                  <Typography variant="h2" gutterBottom>
+                    {wiki.title}
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    {wiki.description}
+                  </Typography>
+                </ListItem>
+              ))}
+            </List>
+          )
+          : <Alert>No wikis found.</Alert>}
+      </div>
+    </MainLayout>
   );
 }
 
