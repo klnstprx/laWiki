473d895d31ff6be4bd919e683ae0a3584e3d97f2
Pablo <pabloruar03@uma.es>
2024-11-30 12:44:37 +0100

    PostEntradaPage y mejor WikiPage

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage.jsx";
 import EntradaPage from "./pages/EntradaPage.jsx";
 import WikiPage from "./pages/WikiPage.jsx";
 import VersionPage from "./pages/VersionPage.jsx";
+import PostEntradaPage from "./pages/PostEntradaPage.jsx";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/entrada" element={<EntradaPage />} />
         <Route path="/wiki" element={<WikiPage />} />
         <Route path="/versiones" element={<VersionPage />} />
+        <Route path="/crear-entrada" element={<PostEntradaPage />} />
         {/* <Route path="/about" element={<About />} /> */}
         {/* <Route path="/articles/:id" element={<Article />} /> */}
       </Routes>
