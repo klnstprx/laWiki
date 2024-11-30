221261ca1c20a5b0fcbfdf8234ee943bef078175
klnstprx <ignacy.borzestowski@gmail.com>
2024-11-30 20:28:55 +0100

    fixed version

--- src/frontend/src/components/Footer.jsx
+++ src/frontend/src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       bgcolor="primary.main"
       color="white"
       textAlign="center"
-      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1000 }}
       elevation={3}
     >
       <Typography variant="body2">
