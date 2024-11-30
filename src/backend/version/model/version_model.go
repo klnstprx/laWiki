21a63ec0670d807bc60961fc1f90a86b5fb5c4fb
klnstprx <ignacy.borzestowski@gmail.com>
2024-11-30 19:31:17 +0100

    createdAt, updatedAt properly set.

--- src/backend/version/model/version_model.go
+++ src/backend/version/model/version_model.go
@@ -7,5 +7,6 @@ type Version struct {
 	Content   string    `json:"content" bson:"content"`
 	Editor    string    `json:"editor" bson:"editor"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 	EntryID   string    `json:"entry_id" bson:"entry_id"`
 }
