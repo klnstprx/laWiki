f28a31f4b97784f5326ac638beb2aaa21846d4be
unknown <crisruizmartin1@gmail.com>
2024-12-06 15:44:37 +0100

    Major overhaul de Form Entrada

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,7 +3,6 @@ import HomePage from "./pages/HomePage.jsx";
 import EntradaPage from "./pages/EntradaPage.jsx";
 import WikiPage from "./pages/WikiPage.jsx";
 import VersionPage from "./pages/VersionPage.jsx";
-import FormEntradaPage from "./pages/FormEntradaPage.jsx";
 import MainLayout from "./layout/MainLayout.jsx";
 import FormVersionPage from "./pages/FormVersionPage.jsx";
 import AdvancedSearchPage from "./pages/AdvancedSearchPage.jsx";
@@ -19,11 +18,8 @@ function App() {
         <Route path="/wiki/form" element={<FormWikiPage />} />
         <Route path="/wiki/form/:wikiId" element={<FormWikiPage />} />
         <Route path="/versiones/:entryId" element={<VersionPage />} />
-        <Route path="/crear-entrada/:id" element={<FormEntradaPage />} />
-        <Route
-          path="/version/form/:entryId/:versionId?"
-          element={<FormVersionPage />}
-        />
+        <Route path="/crear-entrada/:wikiId" element={<FormVersionPage />} />
+        <Route path="/version/form/:entryId/:versionId?" element={<FormVersionPage />} />
         <Route path="/advanced-search" element={<AdvancedSearchPage />} />
         {/* <Route path="/about" element={<About />} /> */}
         {/* <Route path="/articles/:id" element={<Article />} /> */}
