9fdd759823b7c10a84c77c84ac87eacd5daaffec
Olivier <oliviergg@uma.es>
2024-12-21 21:25:38 +0100

    modelo arreglado y añade datos de usuario real

--- src/backend/auth/model/auth_model.go
+++ src/backend/auth/model/auth_model.go
@@ -1,14 +1,14 @@
 package model
 
 type User struct {
-	ID            string  `json:"id"`
-	Email         string  `json:"email"`
-	Name          string  `json:"name"`
-	GivenName     string  `json:"given_name"`
-	FamilyName    string  `json:"family_name"`
-	Picture       string  `json:"picture"`
-	Locale        string  `json:"locale"`
-	VerifiedEmail bool    `json:"verified_email"`
-	Role          string  `json:"role"`
-	Valoration    float64 `json:"valoration"`
+	ID            string  `json:"id" bson:"_id,omitempty"`
+	Email         string  `json:"email" bson:"email"`
+	Name          string  `json:"name" bson:"name"`
+	GivenName     string  `json:"given_name" bson:"given_name"`
+	FamilyName    string  `json:"family_name" bson:"family_name"`
+	Picture       string  `json:"picture" bson:"picture"`
+	Locale        string  `json:"locale" bson:"locale"`
+	VerifiedEmail bool    `json:"verified_email" bson:"verified_email"`
+	Role          string  `json:"role" bson:"role"`
+	Valoration    float64 `json:"valoration" bson:"valoration"`
 }
